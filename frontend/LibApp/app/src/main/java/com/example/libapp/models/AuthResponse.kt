ecd72828f6a7d7965a6f909d981a27bb8eb2d651
Shoolife <ivan14.02.2003@gmail.com>
2024-12-28 03:31:22 +0500

    Старый функционал снова работает, только в новом дизайне

--- frontend/LibApp/app/src/main/java/com/example/libapp/models/AuthResponse.kt
+++ frontend/LibApp/app/src/main/java/com/example/libapp/models/AuthResponse.kt
@@ -3,6 +3,6 @@ package com.example.libapp.models
 data class AuthResponse(
     val status: String,
     val message: String,
-    val userId: String? // Может быть `null`, если `userId` не возвращается
+    val userId: String?
 )
 
--- frontend/LibApp/app/src/main/java/com/example/libapp/models/BookResponse.kt
+++ frontend/LibApp/app/src/main/java/com/example/libapp/models/BookResponse.kt
@@ -3,5 +3,5 @@ package com.example.libapp.models
 data class BookResponse(
     val status: String,
     val message: String,
-    val book: Book? = null // Добавлено поле book
+    val book: Book? = null
 )
